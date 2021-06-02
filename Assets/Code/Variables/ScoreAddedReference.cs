2f7b824e01344c4aefdf0f14377b5bf8e801f045
Zach Hartzog <zach.hartzog@parivedasolutions.com>
2021-06-02 18:25:44 -0500

    Adding the rest of the Scene for Score Updater Example

---
+++ Assets/Code/Variables/ScoreAddedReference.cs
@@ -0,0 +1,13 @@
+﻿// ----------------------------------------------------------------------------
+// 
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+namespace AngryBirds
+{
+    public class ScoreAddedReference : MonoBehaviour
+    {
+        public FloatReference ScoreAdded;
+    }
+}
---
+++ Assets/Code/Variables/ScoreHandler.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------------
+// 
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+namespace AngryBirds
+{
+    public class ScoreHandler : MonoBehaviour
+    {
+        public FloatVariable Score;
+        public FloatVariable StartScore;
+        public Event ScoreChangeEvent;
+
+        private void Start()
+        {
+            Score.SetValue(StartScore);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            Debug.Log("Made it here");
+            ScoreAddedReference scoreChange = other.gameObject.GetComponent<ScoreAddedReference>();
+            if (scoreChange != null)
+                Score.ApplyChange(scoreChange.ScoreAdded);
+                Debug.Log(Score.Value);
+                ScoreChangeEvent.Raise();
+        }
+    }
+}
