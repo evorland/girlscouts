b6d0ad941f4209e0ff7ab36cf2b5ba66dce18121
Sean Blonien <sean.blonien@parivedasolutions.com>
2021-06-05 13:13:13 -0500

    Added SceneEvent Scriptable object which acts like SceneManager

---
+++ Assets/Code/Variables/SceneEvent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace AngryBirds
+{
+    [CreateAssetMenu]
+    public class SceneEvent : ScriptableObject
+    {
+        public void GoNextScene()
+        {
+            var active = SceneManager.GetActiveScene();
+            var next = SceneManager.GetSceneAt(active.buildIndex + 1);
+            SceneManager.LoadScene(next.name);
+        }
+        public void GoPreviousScene()
+        {
+            var active = SceneManager.GetActiveScene();
+            var prev = SceneManager.GetSceneAt(active.buildIndex - 1);
+            SceneManager.LoadScene(prev.name);
+        }
+        public void GoMenu()
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
